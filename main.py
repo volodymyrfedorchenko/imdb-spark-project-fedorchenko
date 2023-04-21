c68943652b019ad02f91508395811de939ead6ce
volodymyrfedorchenko <volodymyr.fedorchenko@nure.ua>
2023-04-21 16:26:46 +0300

    Initial commit

--- main.py
+++ main.py
@@ -4,13 +4,13 @@ from pyspark.sql import SparkSession
 
 def main():
 
-    spark_session = (SparkSession.builder()
+    spark_session = (SparkSession.builder
                      .master('local')
                      .appName('imdb-spark-project-fedorchenko')
                      .config(conf=SparkConf())
                      .getOrCreate())
 
-    name_basics_df = spark_session.read.csv(D:/Fedor/Project/imdb-spark-project-fedorchenko/imdb-data/name.basics.tsv.gz)
+    name_basics_df = spark_session.read.csv('D:/Fedor/Project/imdb-spark-project-fedorchenko/imdb-data/name.basics.tsv.gz')
     name_basics_df.show()
 
 if __name__ == '__main__':
