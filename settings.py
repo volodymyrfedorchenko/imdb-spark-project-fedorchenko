f462e80da9bdc0f89b77016038b41dc079c099c8
volodymyrfedorchenko <volodymyr.fedorchenko@nure.ua>
2023-04-24 19:17:44 +0300

    Initial commit

--- settings.py
+++ settings.py
@@ -1,3 +1,4 @@
 #! Path_of_files
 
 TITLE_EPISODE_PATH = 'D:/Fedor/Project/imdb-spark-project-fedorchenko/imdb-data/title.episode.tsv.gz'
+TITLE_AKAS_PATH = 'D:/Fedor/Project/imdb-spark-project-fedorchenko/imdb-data/title.akas.tsv.gz'
---
+++ task1.py
@@ -0,0 +1,8 @@
+#!
+
+import settings as s
+from read_write import write
+def task1(df):
+    df.filter().withColumn
+
+    write(df, directory)
