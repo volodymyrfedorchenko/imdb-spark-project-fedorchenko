61c8f19da47fc813f4891be265339557ce642b0d
volodymyrfedorchenko <volodymyr.fedorchenko@nure.ua>
2023-04-30 23:07:00 +0300

    Initial commit

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 #!
 import findspark
 from pyspark import SparkConf
-from pyspark.sql import SparkSession
+from pyspark.sql import SparkSession, Window
 import pyspark.sql.types as t
 import pyspark.sql.functions as f
 
@@ -25,6 +25,8 @@ def main():
 
     #Test
     '''
+    #T4
+    
     df_principals = spark_session.read.csv(
         s.TITLE_PRINCIPALS_PATH,
         schema=s.schema_title_principals,
@@ -72,6 +74,55 @@ def main():
     df_name_title_principals = df_name_title_principals.orderBy(str(c.COLUMS_NAME_BASICS[1]))
 
     df_name_title_principals.show(300)
+    
+    #T5
+    
+    df_title_akas_id_region = df_title_akas.filter(f.col(c.COLUMS_TITLE_AKAS[3]) != r'\N').\
+                                            select(df_title_akas[c.COLUMS_TITLE_AKAS[0]],
+                                                   df_title_akas[c.COLUMS_TITLE_AKAS[3]])
+
+    df_title_basics_id_origtitle_adult = df_title_basics.select(df_title_basics[c.COLUMS_TITLE_BASICS[0]],
+                                                                df_title_basics[c.COLUMS_TITLE_BASICS[3]],
+                                                                df_title_basics[c.COLUMS_TITLE_BASICS[4]])
+
+    df_title_ratings_id_rating = df_title_ratings.select(df_title_ratings[c.COLUMNS_TITLE_RATINGS[0]],
+                                                                df_title_ratings[c.COLUMNS_TITLE_RATINGS[1]])
+
+    df_title_basics_id_origtitle_adult_filter = df_title_basics_id_origtitle_adult.filter(f.col(c.COLUMS_TITLE_BASICS[4]) == 1)
+
+    df_title_basics_id_origtitle_filter = df_title_basics_id_origtitle_adult.drop(f.col(c.COLUMS_TITLE_BASICS[4]))
+
+    df_title_akas_id_region_origtitle_filter = df_title_akas_id_region.join(df_title_basics_id_origtitle_filter,
+                                                                            df_title_akas_id_region[c.COLUMS_TITLE_AKAS[0]]
+                                                                            == df_title_basics_id_origtitle_filter[c.COLUMS_TITLE_BASICS[0]])
+
+    df_tconst_region_originalTitle = df_title_akas_id_region_origtitle_filter.select(str(c.COLUMS_TITLE_BASICS[0]),
+                                                                                     str(c.COLUMS_TITLE_AKAS[3]),
+                                                                                     str(c.COLUMS_TITLE_BASICS[3]))
+    # 5_1
+    df_region_count = df_tconst_region_originalTitle.groupBy(str(c.COLUMS_TITLE_AKAS[3])).count()
+    #df_region_count.show(truncate=False)
+
+    df_tconst_region_originalTitle_count = df_tconst_region_originalTitle.join(df_region_count,
+                                                                               str(c.COLUMS_TITLE_AKAS[3]))
+
+    df_tconst_region_originalTitle_count_rating = df_tconst_region_originalTitle_count.join(df_title_ratings_id_rating,
+                                                                               str(c.COLUMS_TITLE_BASICS[0]))
+
+    window = Window.partitionBy('region').orderBy(['count', 'region', 'averageRating'])
+
+    df_tconst_region_originalTitle_count_rating = df_tconst_region_originalTitle_count_rating. \
+                                                                  withColumn('row_number', f.row_number().over(window))
+
+    df_tconst_region_originalTitle_count_rating = df_tconst_region_originalTitle_count_rating.\
+                                               filter(df_tconst_region_originalTitle_count_rating.row_number < 101).\
+                                               orderBy(['count', 'region', 'averageRating'],
+                                               ascending=[False, False, False])
+
+    df_tconst_region_originalTitle_count_rating = df_tconst_region_originalTitle_count_rating.\
+                                         select('region', 'count', 'originalTitle', 'averageRating')
+
+    df_tconst_region_originalTitle_count_rating.show(truncate=False)
     '''
     # Task 1
     '''
@@ -99,6 +150,7 @@ def main():
 
     df = t4.task4(df_principals, df_name, df_title_basics)
     write(df, 'Task4')
+    
     '''
     # Task 5
 
@@ -106,32 +158,9 @@ def main():
     df_title_basics = read(spark_session, s.TITLE_BASICS_PATH, s.schema_title_basics)
     df_title_ratings = read(spark_session, s.TITLE_RATINGS_PATH, s.schema_title_ratings)
 
-    df_title_akas_id_region = df_title_akas.filter(f.col(c.COLUMS_TITLE_AKAS[3]) != r'\N').\
-                                            select(df_title_akas[c.COLUMS_TITLE_AKAS[0]],
-                                                   df_title_akas[c.COLUMS_TITLE_AKAS[3]])
+    df = t5.task5(df_title_akas, df_title_basics, df_title_ratings)
+    write(df, 'Task5')
 
-    df_title_basics_id_origtitle_adult = df_title_basics.select(df_title_basics[c.COLUMS_TITLE_BASICS[0]],
-                                                                df_title_basics[c.COLUMS_TITLE_BASICS[3]],
-                                                                df_title_basics[c.COLUMS_TITLE_BASICS[4]])
-
-    df_title_ratings_id_rating = df_title_ratings.select(df_title_ratings[c.COLUMNS_TITLE_RATINGS[0]],
-                                                                df_title_ratings[c.COLUMNS_TITLE_RATINGS[1]])
-
-    df_title_basics_id_origtitle_adult_filter = df_title_basics_id_origtitle_adult.filter(f.col(c.COLUMS_TITLE_BASICS[4]) == 1)
-
-    df_title_basics_id_origtitle_filter = df_title_basics_id_origtitle_adult.drop(f.col(c.COLUMS_TITLE_BASICS[4]))
-
-    df_title_akas_id_region_origtitle_filter = df_title_akas_id_region.join(df_title_basics_id_origtitle_filter,
-                                                                            df_title_akas_id_region[c.COLUMS_TITLE_AKAS[0]]
-                                                                            == df_title_basics_id_origtitle_filter[c.COLUMS_TITLE_BASICS[0]])
-
-    df_tconst_region_originalTitle = df_title_akas_id_region_origtitle_filter.select(str(c.COLUMS_TITLE_BASICS[0]),
-                                                                                     str(c.COLUMS_TITLE_AKAS[3]),
-                                                                                     str(c.COLUMS_TITLE_BASICS[3]))
-    # 5_1
-    df_tconst_region_originalTitle.groupBy(str(c.COLUMS_TITLE_AKAS[3])).count().show(truncate=False)
-
-    #df_tconst_region_originalTitle.show(truncate=False)
 
 if __name__ == '__main__':
     main()
--- task5.py
+++ task5.py
@@ -2,13 +2,57 @@
 # Get the top 100 of them from the region with the biggest count to the region with the smallest one.
 
 from pyspark import SparkConf
-from pyspark.sql import SparkSession
+from pyspark.sql import SparkSession, Window
 import pyspark.sql.types as t
 import pyspark.sql.functions as f
 
 import columns as c
-def task5(df):
-    df = df.select(f.col(c.COLUMS_TITLE_BASICS[2]), f.col(c.COLUMS_TITLE_BASICS[3])). \
-        where((f.col(c.COLUMS_TITLE_BASICS[6]) > 120)
-              & (f.col(c.COLUMS_TITLE_BASICS[1]) == 'movie'))
-    return df
+def task5(df_title_akas, df_title_basics, df_title_ratings):
+
+    df_title_akas_id_region = df_title_akas.filter(f.col(c.COLUMS_TITLE_AKAS[3]) != r'\N'). \
+        select(df_title_akas[c.COLUMS_TITLE_AKAS[0]],
+               df_title_akas[c.COLUMS_TITLE_AKAS[3]])
+
+    df_title_basics_id_origtitle_adult = df_title_basics.select(df_title_basics[c.COLUMS_TITLE_BASICS[0]],
+                                                                df_title_basics[c.COLUMS_TITLE_BASICS[3]],
+                                                                df_title_basics[c.COLUMS_TITLE_BASICS[4]])
+
+    df_title_ratings_id_rating = df_title_ratings.select(df_title_ratings[c.COLUMNS_TITLE_RATINGS[0]],
+                                                         df_title_ratings[c.COLUMNS_TITLE_RATINGS[1]])
+
+    df_title_basics_id_origtitle_adult_filter = df_title_basics_id_origtitle_adult.\
+                                                                           filter(f.col(c.COLUMS_TITLE_BASICS[4]) == 1)
+
+    df_title_basics_id_origtitle_filter = df_title_basics_id_origtitle_adult.drop(f.col(c.COLUMS_TITLE_BASICS[4]))
+
+    df_title_akas_id_region_origtitle_filter = df_title_akas_id_region.\
+                                                                       join(df_title_basics_id_origtitle_filter,
+                                                                            df_title_akas_id_region[c.COLUMS_TITLE_AKAS[0]]
+                                                                            == df_title_basics_id_origtitle_filter[c.COLUMS_TITLE_BASICS[0]])
+
+    df_tconst_region_originalTitle = df_title_akas_id_region_origtitle_filter.select(str(c.COLUMS_TITLE_BASICS[0]),
+                                                                                     str(c.COLUMS_TITLE_AKAS[3]),
+                                                                                     str(c.COLUMS_TITLE_BASICS[3]))
+    # 5_1
+    df_region_count = df_tconst_region_originalTitle.groupBy(str(c.COLUMS_TITLE_AKAS[3])).count()
+
+    df_tconst_region_originalTitle_count = df_tconst_region_originalTitle.join(df_region_count,
+                                                                               str(c.COLUMS_TITLE_AKAS[3]))
+
+    df_tconst_region_originalTitle_count_rating = df_tconst_region_originalTitle_count.join(df_title_ratings_id_rating,
+                                                                                        str(c.COLUMS_TITLE_BASICS[0]))
+
+    window = Window.partitionBy('region').orderBy(['count', 'region', 'averageRating'])
+
+    df_tconst_region_originalTitle_count_rating = df_tconst_region_originalTitle_count_rating.\
+                                                                   withColumn('row_number', f.row_number().over(window))
+
+    df_tconst_region_originalTitle_count_rating = df_tconst_region_originalTitle_count_rating.\
+                                                 filter(df_tconst_region_originalTitle_count_rating.row_number < 101).\
+                                                 orderBy(['count', 'region', 'averageRating'],
+                                                          ascending=[False, False, False])
+
+    df_tconst_region_originalTitle_count_rating = df_tconst_region_originalTitle_count_rating.\
+                                                             select('region', 'count', 'originalTitle', 'averageRating')
+
+    return df_tconst_region_originalTitle_count_rating
